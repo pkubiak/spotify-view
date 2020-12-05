a8d38affddd892e86cb3b44f65e2b044c5527980
Paweł Kubiak <pawel.kubiak@protonmail.com>
2020-12-05 07:38:38 +0100

    feat: Implemented additional background styles

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const CLIENT_ID = '2788ef9b99b94f508d797cf63afdc445';
 const REDIRECT_URL = window.location.href.indexOf('localhost') == -1 ? 'https:%2F%2Fpkubiak.github.io%2Fspotify%2Findex.html' : 'http:%2F%2Flocalhost%2Findex.html';
 const POLL_INTERVAL = 5000;
+let TOKEN = null;
 
+const STYLES = 4;
+
+let STYLE = 0;
 var last= null, views=[];
+
+function nextStyle() {
+    STYLE = (STYLE+1) % STYLES;
+    last = null;
+    readState();
+}
 
 function getToken() {
     let params = window.location.hash.substr(1).split('&');
@@ -33,10 +43,22 @@ function updateView(data) {
     let artist = document.createElement('h2');
     backdrop.classList.add('backdrop');
     img.src = data.cover;
-    main.style.backgroundImage = 'url('+data.cover+')';
+    let background = null;
+
+    if(STYLE == 0)
+        background = data.cover;
+    else if(STYLE == 1)
+        background = randomGradient({colorRange: 30, saturation: 30});
+    else if(STYLE == 2)
+        background = randomGradient({saturation: 50});
+    else if(STYLE == 3)
+        background = randomGradient({saturation: 70});
+
+    main.style.backgroundImage = 'url('+background+')';
     title.innerText = data.title;
     artist.innerText = data.artists.join(', ');
     document.title = '▶ ' + data.title + ' - ' + data.artists.join(', ');
+    document.querySelector('a.fa-link').href = data.url;
 
     backdrop.appendChild(img);
     backdrop.appendChild(title);
@@ -58,12 +80,13 @@ async function getCurrentlyPlaying(token) {
         let artists = [];
         for(let artist of data.item.artists)
             artists.push(artist.name);
-
+        console.log(data);
         return {
             id: data.item.id,
             title: data.item.name,
             artists: artists,
-            cover: data.item.album.images[0].url
+            cover: data.item.album.images[0].url,
+            url: data.item.external_urls.spotify
         };
         // if(!data.item.id)
         //     return null;
@@ -79,22 +102,41 @@ async function getCurrentlyPlaying(token) {
     });
 }
 
-async function readState(token) {
-    let current = await getCurrentlyPlaying(token);
-//     {artists: ["Faster"],
-// cover: "https://i.scdn.co/image/ab67616d0000b2733fe3e8a32fb622cc86896eb7",
-// id: "18VZZixjjaxDb15otiMmVV",
-// title: "Audi W LPG"};//
+async function readState() {
+    let current = await getCurrentlyPlaying(TOKEN);
     console.log(current);
     updateView(current);
 }
 
 function oninit(){
-    document.querySelector('a').href = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URL}&scope=user-read-currently-playing`;
-    let token = getToken();
+    document.querySelector('a.fa-spotify').href = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URL}&scope=user-read-currently-playing`;
+    TOKEN = getToken();
 
-    if(token) {
-        readState(token);
-        setInterval(function(){readState(token);}, POLL_INTERVAL);
+    if(TOKEN) {
+        readState();
+        setInterval(readState, POLL_INTERVAL);
     }
+}
+
+function randomGradient(config) {
+    let canvas = document.createElement('canvas');
+    canvas.width = canvas.height = 512;
+    let ctx = canvas.getContext('2d');
+    let baseColor = config.baseColor || 360*Math.random();
+    let colorRange = config.colorRange || 100*Math.random();
+    let saturation = config.saturation || (60*Math.random() + 20);
+    let radius = config.radius || 32;
+
+    for(let i=0;i<100;i++) {
+        ctx.beginPath();
+        let color = (baseColor + colorRange*Math.random()) % 360;
+        let lightness = 40 + 30*Math.random();
+        ctx.fillStyle = 'hsl('+color+', '+saturation+'%, '+lightness+'%)';
+        ctx.arc(512*Math.random(), 512*Math.random(), radius*(1+Math.random()), 0, 10);
+        ctx.fill();
+        if(i == 0)
+            ctx.fillRect(0, 0, 512, 512);
+    }
+
+    return canvas.toDataURL();
 }
